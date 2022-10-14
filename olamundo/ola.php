bb7b1f9829c42608000c61d2e6e2c88de6c24203
Rafael Matiola Arceno <96425404+Rafael-Ma@users.noreply.github.com>
2022-10-13 22:08:08 -0300

    Initial commit

---
+++ olamundo/ola.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Testando PHP</title>
+        <style>
+            h2 {
+                display: inline;
+                background-color: blueviolet;
+                padding: 10px;
+                border-radius: 10px;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+       <h1>Testando PHP</h1>
+       
+       <?php 
+            echo "<h2>Olá mundo!</h2>";
+       ?>
+    </body>
+</html>
